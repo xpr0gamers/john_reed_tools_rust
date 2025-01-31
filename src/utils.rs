e27dd81502de8ee463fe71601d61fb4ac58331bd
Maxi Dietz <maxidietz96@gmail.com>
2025-02-01 00:28:43 +0100

    Verbessere Fehlermeldung für fehlende Einstellungen
    Die Fehlermeldung wurde aktualisiert, um die Pfade anzuzeigen,
    in denen nach der settings.json gesucht wurde. Dies erleichtert
    das Debugging und die Fehlersuche, falls die Datei nicht gefunden
    werden kann.

--- src/utils.rs
+++ src/utils.rs
@@ -11,7 +11,10 @@ pub fn get_app_state() -> Result<AppState, Box<dyn Error>> {
     let settings_path = potential_settings_path
         .iter()
         .find(|path| Path::new(path).exists())
-        .ok_or("settings.json not found")?;
+        .ok_or(format!(
+            "settings.json not found at paths: {:#?}",
+            potential_settings_path
+        ))?;
 
     let file = File::open(settings_path)?;
     let app_state: AppState = serde_json::from_reader(BufReader::new(file))?;
