890ffa6662c64e2a3b3fa75e9fe68c7bc3153584
Maxi Dietz <maxidietz96@gmail.com>
2025-01-31 22:43:54 +0100

    Initial project setup with Rust and configuration files
    - Added Cargo.toml for project dependencies and metadata.
    - Created initial source files including main.rs and types.rs.
    - Configured IDE settings and ignored files for .gitignore.
    - Set up VSCode launch configuration for debugging.

---
+++ src/fetch.rs
@@ -0,0 +1,208 @@
+use std::sync::Arc;
+
+use chrono::{DateTime, FixedOffset};
+use reqwest::cookie::Jar;
+use serde::{Deserialize, Serialize};
+
+pub struct JohnReedApi {
+    client: reqwest::Client,
+}
+
+impl JohnReedApi {
+    pub fn new() -> Self {
+        JohnReedApi {
+            client: reqwest::Client::builder()
+                .cookie_store(true)
+                .cookie_provider(Arc::new(Jar::default()))
+                .build()
+                .unwrap(),
+        }
+    }
+
+    pub async fn login(
+        &self,
+        payload: JohnReedLoginPayload,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let response = self
+            .client
+            .post("https://my.johnreed.fitness/login")
+            .basic_auth(payload.username.to_owned(), payload.password.to_owned())
+            .header("X-Nox-Client-Type", "WEB")
+            .send()
+            .await?;
+
+        if !response.status().is_success() {
+            return Result::Err("Unauthorized".into());
+        }
+
+        Result::Ok(())
+    }
+
+    pub async fn get_user(&self) -> Result<JohnReedMe, Box<dyn std::error::Error>> {
+        let response = self
+            .client
+            .get("https://my.johnreed.fitness/v1/me/info")
+            .header("X-Nox-Client-Type", "WEB")
+            .header(
+                "X-Public-Facility-Group",
+                "JOHNREED-65A11AB8FA704F88B2D8EF52523C576A",
+            )
+            .send()
+            .await?;
+
+        if !response.status().is_success() {
+            return Result::Err("failed get user".into());
+        }
+
+        let john_reed_me: JohnReedMe = response.json().await?;
+        Result::Ok(john_reed_me)
+    }
+
+    pub async fn get_home_studio(&self) -> Result<JohnReedHomeStudio, Box<dyn std::error::Error>> {
+        let response = self
+            .client
+            .get("https://my.johnreed.fitness/nox/v1/studios/home")
+            .header("X-Nox-Client-Type", "WEB")
+            .header(
+                "X-Public-Facility-Group",
+                "JOHNREED-65A11AB8FA704F88B2D8EF52523C576A",
+            )
+            .send()
+            .await?;
+
+        if !response.status().is_success() {
+            return Result::Err("failed get home studio".into());
+        }
+
+        let john_reed_home_studio: JohnReedHomeStudio = response.json().await?;
+        Result::Ok(john_reed_home_studio)
+    }
+
+    pub async fn get_bookable_courses(
+        &self,
+        params: BookableCoursesParams,
+    ) -> Result<Vec<JohnReedCourse>, Box<dyn std::error::Error>> {
+        let mut url =
+            "https://my.johnreed.fitness/nox/v2/bookableitems/courses/with-canceled?".to_string();
+        url += &format!("startDate={}", params.start_date.format("%Y-%m-%d"));
+        url += &params.end_date.map_or("".to_string(), |end_date| {
+            format!("&endDate={}", end_date.format("%Y-%m-%d"))
+        });
+        url += &params
+            .organization_unit_ids
+            .map_or("".to_string(), |organization_unit_ids| {
+                format!("&organizationUnitIds={}", organization_unit_ids)
+            });
+
+        let response = self
+            .client
+            .get(url)
+            .header("X-Nox-Client-Type", "WEB")
+            .header(
+                "X-Public-Facility-Group",
+                "JOHNREED-65A11AB8FA704F88B2D8EF52523C576A",
+            )
+            .send()
+            .await?;
+
+        if !response.status().is_success() {
+            return Result::Err(
+                format!(
+                    "failed to get bookable courses with status {}",
+                    response.status()
+                )
+                .into(),
+            );
+        }
+
+        let john_reed_bookable_courses: Vec<JohnReedCourse> = response.json().await?;
+        Result::Ok(john_reed_bookable_courses)
+    }
+}
+
+#[derive(Debug)]
+pub struct JohnReedLoginPayload {
+    pub username: String,
+    pub password: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct BookableCoursesParams {
+    pub start_date: DateTime<FixedOffset>,
+    pub end_date: Option<DateTime<FixedOffset>>,
+    pub organization_unit_ids: Option<i64>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct JohnReedMe {
+    pub id: i64,
+    pub name: String,
+    pub email: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct JohnReedHomeStudio {
+    pub id: i64,
+    pub name: String,
+    pub email: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct JohnReedCourse {
+    pub id: i64,
+    pub name: String,
+    pub description: Option<String>,
+    #[serde(rename = "appointmentStatus")]
+    pub appointment_status: String,
+    pub slots: Vec<JohnReedSlot>,
+}
+
+impl JohnReedCourse {
+    pub fn is_bookable(&self) -> bool {
+        return self.appointment_status != "CANCELED";
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct JohnReedSlot {
+    #[serde(rename = "startDateTime", with = "chrono_datetime_fixed_offset")]
+    pub start_date_time: Option<DateTime<FixedOffset>>,
+    #[serde(rename = "endDateTime", with = "chrono_datetime_fixed_offset")]
+    pub end_date_time: Option<DateTime<FixedOffset>>,
+    #[serde(rename = "alreadyBooked")]
+    pub already_booked: bool,
+    pub bookable: bool,
+    #[serde(
+        rename = "earliestBookingDateTime",
+        with = "chrono_datetime_fixed_offset"
+    )]
+    pub earliest_booking_date_time: Option<DateTime<FixedOffset>>,
+}
+
+pub mod chrono_datetime_fixed_offset {
+    use chrono::{DateTime, FixedOffset};
+    use serde::{self, Deserialize, Deserializer};
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<DateTime<FixedOffset>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let deserialize_option = Option::<String>::deserialize(deserializer)?;
+        if deserialize_option.is_none() {
+            return Ok(None);
+        }
+        let date_string = deserialize_option.unwrap();
+
+        // Split into ISO date-time and region
+        let (iso_part, _region_part) =
+            date_string.split_at(date_string.find('[').ok_or_else(|| {
+                serde::de::Error::custom("Expected region in format [Region/Zone]")
+            })?);
+
+        // Parse ISO date-time
+        let datetime = DateTime::parse_from_rfc3339(iso_part)
+            .map_err(|err| serde::de::Error::custom(format!("Invalid datetime: {}", err)))?;
+
+        return Result::Ok(Some(datetime));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+pub mod fetch;
+
+use std::{error::Error, fs::File, io::BufReader, time::Duration};
+use tokio::time::interval;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut interval = interval(Duration::from_secs(5));
+    loop {
+        interval.tick().await;
+        check_for_new_courses().await;
+    }
+}
+
+async fn check_for_new_courses() {
+    let file = File::open("settings.json").expect("Settings file not found");
+    let app_state: app_state::AppState = serde_json::from_reader(BufReader::new(file)).unwrap();
+
+    println!(
+        "⏳Checking for new courses to book at: {:?}",
+        chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+    );
+
+    let john_reed_api = fetch::JohnReedApi::new();
+
+    for user in app_state.users {
+        let login_result = john_reed_api
+            .login(fetch::JohnReedLoginPayload {
+                username: user.username.clone(),
+                password: Some(user.password.clone()),
+            })
+            .await;
+        match login_result {
+            Ok(_) => println!("Logged in successfully as: {}", user.username),
+            Err(e) => {
+                println!("Failed to login as: {}", user.username);
+                println!("{:?}", e);
+                continue;
+            }
+        }
+
+        let home_studio_result = john_reed_api.get_home_studio().await;
+        let home_studio = match home_studio_result {
+            Ok(home_studio) => {
+                println!("Home studio: {}", home_studio.name);
+                home_studio
+            }
+            Err(e) => {
+                println!("Failed to get home studio for: {}", user.username);
+                println!("{:?}", e);
+                continue;
+            }
+        };
+
+        let bookable_courses_params = fetch::BookableCoursesParams {
+            start_date: chrono::Local::now().fixed_offset(),
+            end_date: Some(
+                (chrono::Local::now() + Duration::from_secs(60 * 60 * 24 * 7)).fixed_offset(),
+            ),
+            organization_unit_ids: Some(home_studio.id),
+        };
+        let bookable_courses_result = john_reed_api
+            .get_bookable_courses(bookable_courses_params)
+            .await;
+        let bookable_courses = match bookable_courses_result {
+            Ok(bookable_courses) => bookable_courses,
+            Err(e) => {
+                println!("Failed to get bookable courses for: {}", user.username);
+                println!("{:?}", e);
+                continue;
+            }
+        };
+
+        for course in user.courses {
+            let bookable_course = bookable_courses
+                .iter()
+                .find(|bookable_course| bookable_course.name == course.name);
+            let bookable_course = match bookable_course {
+                Some(bookable_course) => bookable_course,
+                None => {
+                    continue;
+                }
+            };
+
+            if !bookable_course.is_bookable() {
+                continue;
+            }
+
+            for time_slot in course.time_slots {
+                println!(
+                    "Checking for course: {} on {} from {}",
+                    course.name, time_slot.start_time, time_slot.end_time
+                );
+            }
+        }
+    }
+}
+
+mod app_state {
+
+    use serde::Deserialize;
+
+    #[derive(Debug, Deserialize, Clone)]
+    pub struct AppState {
+        pub users: Vec<User>,
+    }
+
+    #[derive(Debug, Deserialize, Clone)]
+    pub struct User {
+        pub username: String,
+        pub password: String,
+        pub courses: Vec<CourseToBook>,
+    }
+
+    #[derive(Debug, Deserialize, Clone)]
+    pub struct CourseToBook {
+        pub name: String,
+        #[serde(rename = "timeSlots")]
+        pub time_slots: Vec<TimeSlotToBook>,
+    }
+
+    #[derive(Debug, Deserialize, Clone)]
+    pub struct TimeSlotToBook {
+        pub day: Day,
+        #[serde(rename = "startTime")]
+        pub start_time: String,
+        #[serde(rename = "endTime")]
+        pub end_time: String,
+    }
+
+    #[derive(Debug, Deserialize, Clone, Copy, PartialEq, Eq)]
+    pub enum Day {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+    }
+}
